ee7a8814e38e06c44d8f0dc50a106eec11dde3cb
Wojciech Kloska <shangteus@ixsystems.com>
2015-12-11 16:34:39 +0100

    FIXED: feed my OCD fixing ResourceError properly

--- src/dispatcher/client/python/freenas/dispatcher/client_transport.py
+++ src/dispatcher/client/python/freenas/dispatcher/client_transport.py
@@ -433,9 +433,7 @@ class ClientTransportSock(ClientTransportBase):
             recv_t.setDaemon(True)
             recv_t.start()
         except socket.error as err:
-            self.sock.close()
-            self.fd.close()
-            self.terminated = True
+            self.closed()
             debug_log('Socket connection exception: {0}', err)
             raise
 
