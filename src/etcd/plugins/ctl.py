09202b5f3b606098a37f8f7133e008a1d2d7ad87
Jakub Klama <jakub@ixsystems.com>
2016-01-08 19:06:28 +0100

    Add support for file-based iSCSI extents.

--- src/etcd/plugins/ctl.py
+++ src/etcd/plugins/ctl.py
@@ -70,7 +70,7 @@ def generate_luns(context):
     for share in context.client.call_sync('share.query', [('type', '=', 'iscsi')]):
         props = share['properties']
         extent = {
-            'path': os.path.join('/dev/zvol', share['dataset_path']),
+            'path': share['filesystem_path'],
             'blocksize': props['block_size'],
             'serial': props['serial'],
             'options': {
