87770cf9af8e50b551db2e097c254fbfacf9dc0b
William Grzybowski <wg@FreeBSD.org>
2016-02-10 10:56:40 -0200

    Add group CRUD

--- src/restd/plugins/account.py
+++ src/restd/plugins/account.py
@@ -1,9 +1,14 @@
 from base import CRUDBase
 
 
+class GroupCRUD(CRUDBase):
+    namespace = 'group'
+
+
 class UserCRUD(CRUDBase):
     namespace = 'user'
 
 
 def _init(rest):
+    rest.register_crud(GroupCRUD)
     rest.register_crud(UserCRUD)
