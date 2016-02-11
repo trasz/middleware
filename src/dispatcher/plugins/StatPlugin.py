6c2f44568b67e9ad79dae21ea2ffc0934db937b0
Wojciech Kloska <shangteus@ixsystems.com>
2016-02-11 01:51:01 +0100

    ADDED: statistics have units now

--- src/dispatcher/plugins/StatPlugin.py
+++ src/dispatcher/plugins/StatPlugin.py
@@ -33,6 +33,45 @@ from task import Provider, Task, VerifyException, TaskException, query
 from freenas.utils.query import wrap
 
 
+UNITS = {
+    'Ops/s': {
+        'match': lambda x: re.match(r'(.*)(disk_merged|disk_ops)(.*)', x),
+        'normalize': lambda x: x,
+        'raw': lambda x: x
+    },
+    'B/s': {
+        'match': lambda x: re.match(r'(.*)(disk_octets|if_octets)(.*)', x),
+        'normalize': lambda x: x,
+        'raw': lambda x: x
+    },
+    'B': {
+        'match': lambda x: re.match(r'(.*)(df-|memory)(.*)', x),
+        'normalize': lambda x: x,
+        'raw': lambda x: x
+    },
+    'C': {
+        'match': lambda x: re.match(r'(.*)(temperature)(.*)', x),
+        'normalize': lambda x: None if x == -1 else (x - 2732)/10,
+        'raw': lambda x: x * 10 + 2732
+    },
+    'Jiffies': {
+        'match': lambda x: re.match(r'(.*)(cpu-)(.*)', x),
+        'normalize': lambda x: x,
+        'raw': lambda x: x
+    },
+    'Packets/s': {
+        'match': lambda x: re.match(r'(.*)(if_packets)(.*)', x),
+        'normalize': lambda x: x,
+        'raw': lambda x: x
+    },
+    'Errors/s': {
+        'match': lambda x: re.match(r'(.*)(if_errors)(.*)', x),
+        'normalize': lambda x: x,
+        'raw': lambda x: x
+    }
+}
+
+
 class StatProvider(Provider):
     @query('stat')
     def query(self, filter=None, params=None):
@@ -52,6 +91,8 @@ class CpuStatProvider(Provider):
             else:
                 stat['short_name'] = 'cpu-' + re.search(r'\d+', stat['name']).group() + '--' + type
 
+            stat['unit'], stat['normalized_value'] = normalize(stat['name'], stat['last_value'])
+
         return wrap(stats).query(*(filter or []), **(params or {}))
 
 
@@ -64,6 +105,8 @@ class DiskStatProvider(Provider):
             split_name = stat['name'].split('.', 3)
             stat['short_name'] = split_name[1] + '--' + split_name[3] + '-' + split_name[2].split('_', 2)[1]
 
+            stat['unit'], stat['normalized_value'] = normalize(stat['name'], stat['last_value'])
+
         return wrap(stats).query(*(filter or []), **(params or {}))
 
 
@@ -75,6 +118,8 @@ class NetworkStatProvider(Provider):
         for stat in stats:
             split_name = stat['name'].split('.', 3)
             stat['short_name'] = split_name[1] + '--' + split_name[3] + '-' + split_name[2].split('_', 2)[1]
+
+            stat['unit'], stat['normalized_value'] = normalize(stat['name'], stat['last_value'])
 
         return wrap(stats).query(*(filter or []), **(params or {}))
 
@@ -99,6 +144,8 @@ class SystemStatProvider(Provider):
             else:
                 stat['short_name'] = split_name[2]
 
+            stat['unit'], stat['normalized_value'] = normalize(stat['name'], stat['last_value'])
+
         return wrap(stats).query(*(filter or []), **(params or {}))
 
 
@@ -113,18 +160,50 @@ class UpdateAlertTask(Task):
         updated_alerts = stat.get('alerts')
 
         if 'alert_high' in updated_alerts:
-            self.dispatcher.call_sync('statd.alert.set_high_value', name, updated_alerts['alert_high'])
+            self.dispatcher.call_sync(
+                'statd.alert.set_high_value',
+                name,
+                raw(name, updated_alerts['alert_high'])
+            )
         if 'alert_high_enabled' in updated_alerts:
-            self.dispatcher.call_sync('statd.alert.set_high_enabled', name, updated_alerts['alert_high_enabled'])
+            self.dispatcher.call_sync(
+                'statd.alert.set_high_enabled',
+                name,
+                raw(name, updated_alerts['alert_high_enabled'])
+            )
         if 'alert_low' in updated_alerts:
-            self.dispatcher.call_sync('statd.alert.set_low_value', name, updated_alerts['alert_low'])
+            self.dispatcher.call_sync(
+                'statd.alert.set_low_value',
+                name,
+                raw(name, updated_alerts['alert_low'])
+            )
         if 'alert_low_enabled' in updated_alerts:
-            self.dispatcher.call_sync('statd.alert.set_low_enabled', name, updated_alerts['alert_low_enabled'])
+            self.dispatcher.call_sync(
+                'statd.alert.set_low_enabled',
+                name,
+                raw(name, updated_alerts['alert_low_enabled'])
+            )
 
-        self.dispatcher.dispatch_event('stat.changed', {
+        self.dispatcher.dispatch_event('stat.alert.changed', {
             'operation': 'update',
             'ids': [name]
         })
+
+
+def normalize(name, value):
+    for key, unit in UNITS.items():
+        if unit['match'](name):
+            return key, unit['normalize'](value)
+
+    return '', value
+
+
+def raw(name, value):
+    for key, unit in UNITS.items():
+        if unit['match'](name):
+            return unit['raw'](value)
+
+    return value
 
 
 def _init(dispatcher, plugin):
@@ -134,5 +213,5 @@ def _init(dispatcher, plugin):
     plugin.register_provider('stat.network', NetworkStatProvider)
     plugin.register_provider('stat.system', SystemStatProvider)
     plugin.register_task_handler('stat.alert_update', UpdateAlertTask)
-    plugin.register_event_type('stat.changed')
+    plugin.register_event_type('stat.alert.changed')
 
