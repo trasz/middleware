96279112f44a23191534d8fbea0e737198f6251e
William Grzybowski <wg@FreeBSD.org>
2016-02-12 10:32:56 -0200

    Add a basic extendable resource class

--- src/restd/src/base.py
+++ src/restd/src/base.py
@@ -5,6 +5,88 @@ from freenas.dispatcher.rpc import RpcException
 from swagger import normalize_schema
 
 
+class Task(object):
+
+    name = None
+
+    def __init__(self, resource, dispatcher, name=None):
+        if name is not None:
+            self.name = name
+        self.resouce = resource
+        self.dispatcher = dispatcher
+
+    def run(self, *args, **kwargs):
+        try:
+            result = self.dispatcher.call_task_sync(self.name, *args, **kwargs)
+        except RpcException as e:
+            raise falcon.HTTPBadRequest(e.message, str(e))
+        if result['state'] != 'FINISHED':
+            if result['error']:
+                title = result['error']['type']
+                message = result['error']['message']
+            else:
+                title = 'UnknownError'
+                message = 'Failed to create, check task #{0}'.format(result['id'])
+            raise falcon.HTTPBadRequest(title, message)
+        return result
+
+
+class Resource(object):
+
+    name = None
+    parent = None
+
+    get = None
+    post = None
+    put = None
+    delete = None
+
+    def __init__(self, rest, parent=None):
+        self.rest = rest
+        self.children = []
+
+        if parent is not None:
+            parent.add_child(self)
+
+        self.rest.api.add_route(self.get_uri(), self)
+
+    def __getattr__(self, attr):
+        if attr in ('on_get', 'on_post', 'on_delete', 'on_put'):
+            do = object.__getattribute__(self, 'do')
+            method = attr.split('_')[-1]
+
+            if object.__getattribute__(self, method) is None:
+                return None
+
+            def on_method(req, resp):
+                return do(method, req, resp)
+
+            return on_method
+        return object.__getattribute__(self, attr)
+
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+    def get_uri(self):
+        if self.parent is None:
+            return '/{0}'.format(self.name)
+        return '{0}/{1}'.format(self.parent.get_uri(), self.name)
+
+    def do(self, method, req, resp, **kwargs):
+        method_op = getattr(self, method)
+        if ':' not in method_op:
+            raise falcon.HTTPError(
+                falcon.HTTP_500, 'Internal Error', 'No valid operation provided'
+            )
+        type_, name = method_op.split(':', 1)
+        assert type_ in ('task', 'rpc')
+
+        if type_ == 'task':
+            t = Task(self, req.context['client'], name=name)
+            t.run(req.context['doc'])
+
+
 class EntityResource(object):
 
     def __init__(self, rest, dispatcher, crud):
@@ -138,14 +220,14 @@ class ItemResource(object):
             ],
         }
 
-    def get_entry(self, id):
-        entry = req.context['client'].call_sync(self.crud.get_retrieve_method_name(), [('id', '=', int(id))], {'single': True})
+    def get_entry(self, client, id):
+        entry = client.call_sync(self.crud.get_retrieve_method_name(), [('id', '=', int(id))], {'single': True})
         if entry is None:
             raise falcon.HTTPNotFound
         return entry
 
     def on_get(self, req, resp, id):
-        req.context['result'] = self.get_entry(id)
+        req.context['result'] = self.get_entry(req.context['client'], id)
 
     def on_put(self, req, resp, id):
         entry = self.get_entry(id)
@@ -190,6 +272,9 @@ class CRUDBase(object):
 
         rest.api.add_route(self.entity.get_uri(), self.entity)
         rest.api.add_route(self.item.get_uri(), self.item)
+
+    def add_child(self, child):
+        child.parent = self
 
     def get_uri(self):
         return '/{0}'.format(self.namespace)
