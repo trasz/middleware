c472fdb977e70bc4bbd4a60160d7765e4cfaa51c
William Grzybowski <wg@FreeBSD.org>
2016-02-16 16:27:34 -0200

    Register FTP service

--- src/restd/plugins/service.py
+++ src/restd/plugins/service.py
@@ -1,10 +1,14 @@
 from base import ServiceBase
 
 
+class FTPService(ServiceBase):
+    namespace = 'ftp'
+
+
 class SSHService(ServiceBase):
     namespace = 'ssh'
 
 
 def _init(rest):
+    rest.register_service(FTPService)
     rest.register_service(SSHService)
-    SSHService(rest, rest.dispatcher)
