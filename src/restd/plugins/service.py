560e71ebd83403df924a7d6d6ef7edb893fbf185
William Grzybowski <wg@FreeBSD.org>
2016-02-18 13:00:43 -0200

    Add a few services more

--- src/restd/plugins/service.py
+++ src/restd/plugins/service.py
@@ -1,8 +1,20 @@
 from base import ServiceBase
 
 
+class AFPService(ServiceBase):
+    namespace = 'afp'
+
+
+class DynDNSService(ServiceBase):
+    namespace = 'dyndns'
+
+
 class FTPService(ServiceBase):
     namespace = 'ftp'
+
+
+class NFSService(ServiceBase):
+    namespace = 'nfs'
 
 
 class SSHService(ServiceBase):
@@ -10,5 +22,8 @@ class SSHService(ServiceBase):
 
 
 def _init(rest):
+    rest.register_service(AFPService)
+    rest.register_service(DynDNSService)
     rest.register_service(FTPService)
+    rest.register_service(NFSService)
     rest.register_service(SSHService)
