6e502d3a1ca21509cca1bea805574191ee2e719d
William Grzybowski <wg@FreeBSD.org>
2016-02-22 14:13:43 -0300

    Add network plugin with interface CRUD

---
+++ src/restd/plugins/network.py
@@ -0,0 +1,42 @@
+from base import CRUDBase, Resource
+
+
+class InterfaceDownResource(Resource):
+    name = 'down'
+    post = 'task:network.interface.down'
+
+    def run_post(self, req, kwargs):
+        return [kwargs['id']], {}
+
+
+class InterfaceUpResource(Resource):
+    name = 'up'
+    post = 'task:network.interface.up'
+
+    def run_post(self, req, kwargs):
+        return [kwargs['id']], {}
+
+
+class InterfaceRenewResource(Resource):
+    name = 'up'
+    post = 'task:network.interface.renew'
+
+    def run_post(self, req, kwargs):
+        return [kwargs['id']], {}
+
+
+class InterfaceCRUD(CRUDBase):
+    name = 'network/interface'
+    namespace = 'network.interface'
+    item_resources = (
+        InterfaceDownResource,
+        InterfaceUpResource,
+        InterfaceRenewResource,
+    )
+
+    def get_update_method_name(self):
+        return '{0}.configure'.format(self.namespace)
+
+
+def _init(rest):
+    rest.register_crud(InterfaceCRUD)
