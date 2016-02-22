751e058c68795a62c0985958910a1c7d512d980b
William Grzybowski <wg@FreeBSD.org>
2016-02-22 17:25:09 -0300

    Add a few services more to the plugin

--- src/restd/plugins/service.py
+++ src/restd/plugins/service.py
@@ -17,8 +17,32 @@ class NFSService(ServiceBase):
     namespace = 'nfs'
 
 
+class SMARTDService(ServiceBase):
+    namespace = 'smb'
+
+
+class SMBService(ServiceBase):
+    namespace = 'smb'
+
+
+class SNMPService(ServiceBase):
+    namespace = 'snmp'
+
+
 class SSHService(ServiceBase):
     namespace = 'ssh'
+
+
+class TFTPService(ServiceBase):
+    namespace = 'tftp'
+
+
+class UPSService(ServiceBase):
+    namespace = 'ups'
+
+
+class WebDAVService(ServiceBase):
+    namespace = 'webdav'
 
 
 def _init(rest):
@@ -26,4 +50,10 @@ def _init(rest):
     rest.register_service(DynDNSService)
     rest.register_service(FTPService)
     rest.register_service(NFSService)
+    rest.register_service(SMARTDService)
+    rest.register_service(SMBService)
+    rest.register_service(SNMPService)
     rest.register_service(SSHService)
+    rest.register_service(TFTPService)
+    rest.register_service(UPSService)
+    rest.register_service(WebDAVService)
