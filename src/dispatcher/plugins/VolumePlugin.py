f6a2407d91727048dda5fe3d91aba3867ea8472f
Jakub Klama <jakub@ixsystems.com>
2016-03-16 02:22:06 +0100

    Comment out automatic UUID assignment until related py-libzfs bug is fixed.

--- src/dispatcher/plugins/VolumePlugin.py
+++ src/dispatcher/plugins/VolumePlugin.py
@@ -1942,12 +1942,12 @@ def _init(dispatcher, plugin):
         except (ValueError, TypeError):
             pass
 
-        uuid = snapshot.get('properties.org\\.freenas:uuid.value')
-        uuid_source = snapshot.get('properties.org\\.freenas:uuid.source')
-        if not uuid or uuid_source == 'INHERITED':
-            dispatcher.submit_task('zfs.update', pool, snapshot['name'], {
-                'org.freenas:uuid': {'value': str(uuid.uuid4())}
-            })
+        #uuid = snapshot.get('properties.org\\.freenas:uuid.value')
+        #uuid_source = snapshot.get('properties.org\\.freenas:uuid.source')
+        #if not uuid or uuid_source == 'INHERITED':
+        #    dispatcher.submit_task('zfs.update', pool, snapshot['name'], {
+        #        'org.freenas:uuid': {'value': str(uuid.uuid4())}
+        #    })
 
         return {
             'id': snapshot['name'],
@@ -1976,12 +1976,12 @@ def _init(dispatcher, plugin):
             except RpcException:
                 pass
 
-        uuid = ds.get('properties.org\\.freenas:uuid.value')
-        uuid_source = ds.get('properties.org\\.freenas:uuid.source')
-        if not uuid or uuid_source == 'INHERITED':
-            dispatcher.submit_task('zfs.update', ds['pool'], ds['name'], {
-                'org.freenas:uuid': {'value': str(uuid.uuid4())}
-            })
+        #uuid = ds.get('properties.org\\.freenas:uuid.value')
+        #uuid_source = ds.get('properties.org\\.freenas:uuid.source')
+        #if not uuid or uuid_source == 'INHERITED':
+        #    dispatcher.submit_task('zfs.update', ds['pool'], ds['name'], {
+        #        'org.freenas:uuid': {'value': str(uuid.uuid4())}
+        #    })
 
         return {
             'id': ds['name'],
