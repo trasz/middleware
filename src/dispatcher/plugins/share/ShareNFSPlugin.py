40d51417c48ddbf18ddfecbaec42d26af782f2c6
Jakub Klama <jakub@ixsystems.com>
2016-03-18 01:39:38 +0100

    Move misplaced code.

--- src/dispatcher/plugins/share/ShareNFSPlugin.py
+++ src/dispatcher/plugins/share/ShareNFSPlugin.py
@@ -64,15 +64,14 @@ class NFSSharesProvider(Provider):
 @accepts(h.ref('share'))
 class CreateNFSShareTask(Task):
     def describe(self, share):
-        properties = share['properties']
+        return "Creating NFS share {0}".format(share['name'])
 
+    def verify(self, share):
+        properties = share['properties']
         if (properties.get('maproot_user') or properties.get('maproot_group')) and \
            (properties.get('mapall_user') or properties.get('mapall_group')):
             raise VerifyException(errno.EINVAL, 'Cannot set maproot and mapall properties simultaneously')
 
-        return "Creating NFS share {0}".format(share['name'])
-
-    def verify(self, share):
         return ['service:nfs']
 
     def run(self, share):
@@ -146,15 +145,14 @@ class DeleteNFSShareTask(Task):
 @accepts(h.ref('share'))
 class ImportNFSShareTask(CreateNFSShareTask):
     def describe(self, share):
-        properties = share['properties']
+        return "Importing NFS share {0}".format(share['name'])
 
+    def verify(self, share):
+        properties = share['properties']
         if (properties.get('maproot_user') or properties.get('maproot_group')) and \
            (properties.get('mapall_user') or properties.get('mapall_group')):
             raise VerifyException(errno.EINVAL, 'Cannot set maproot and mapall properties simultaneously')
 
-        return "Importing NFS share {0}".format(share['name'])
-
-    def verify(self, share):
         return super(ImportNFSShareTask, self).verify(share)
 
     def run(self, share):
