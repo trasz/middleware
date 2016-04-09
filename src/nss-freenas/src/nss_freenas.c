d50480a9660ab80599355b2e580fc5ab33c48c70
Jakub Klama <jakub@ixsystems.com>
2016-04-09 22:40:38 +0200

    First pass at gethostbyname2_r() implementation.

--- src/nss-freenas/src/nss_freenas.c
+++ src/nss-freenas/src/nss_freenas.c
@@ -31,17 +31,21 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/param.h>
+#include <arpa/inet.h>
 #include <pwd.h>
 #include <grp.h>
 #include <nsswitch.h>
 #include <jansson.h>
 #include <dispatcher.h>
+#include <netdb.h>
 
 #define PASSWD_FILE "/etc/passwd.json"
 #define GROUP_FILE  "/etc/group.json"
 
 static char *alloc_string(char **, size_t *, const char *);
+static void *alloc_blob(char **, size_t *, size_t);
 static void *alloc_null(char **, size_t *);
 static void flat_load_files();
 static json_t *flat_find_user(const char *, const char *, uid_t);
@@ -93,6 +97,20 @@ alloc_string(char **buf, size_t *max, const char *str)
 	*buf += length;
 	*max -= length;
 
+	return (ret);
+}
+
+static void *
+alloc_blob(char **buf, size_t *max, size_t length)
+{
+	char *ret;
+
+	if (length > *max)
+		return (NULL);
+
+	ret = *buf;
+	*buf += length;
+	*max -= length;
 	return (ret);
 }
 
@@ -625,19 +643,103 @@ nss_freenas_getgroupmembership(void *retval, void *mdata, va_list ap)
 int
 nss_freenas_getaddrinfo(void *retval, void *mdata, va_list ap)
 {
+	const char *hostname;
+	const char *servname;
+	const struct addrinfo *hints;
+	struct addrinfo **res;
+	int *ret;
+
+	hostname = va_arg(ap, const char *);
+	servname = va_arg(ap, const char *);
+	hints = va_arg(ap, const struct addrinfo *);
+	res = va_arg(ap, struct addrinfo **);
+	ret = va_arg(ap, int *);
+
 	return (NS_UNAVAIL);
 }
 
 int
 nss_freenas_gethostbyaddr_r(void *retval, void *mdata, va_list ap)
 {
+	const void *addr;
+	socklen_t len;
+	int af, ret;
+
+	addr = va_arg(ap, const void *);
+	len = va_arg(ap, socklen_t);
+	ret = va_arg(ap, int);
+
 	return (NS_UNAVAIL);
 }
 
 int
 nss_freenas_gethostbyname2_r(void *retval, void *mdata, va_list ap)
 {
-	return (NS_UNAVAIL);
+	char *buf;
+	const char *name;
+	int *h_errnop;
+	struct hostent *hp;
+	struct hostent **resultp;
+	int af;
+	size_t buflen;
+	int ret_errno;
+	json_t *result, *addrs, *aliases, *i;
+	size_t idx;
+	in_addr_t addr;
+
+	name = va_arg(ap, char *);
+	af = va_arg(ap, int);
+	hp = va_arg(ap, struct hostent *);
+	buf = va_arg(ap, char *);
+	buflen = va_arg(ap, size_t);
+	ret_errno = va_arg(ap, int);
+	h_errnop = va_arg(ap, int *);
+	resultp = (struct hostent **)retval;
+
+	if (call_dispatcher("dscached.host.gethostbyname", json_pack("[si]",
+	    name, af), &result, true) < 0) {
+		return (NS_UNAVAIL);
+	}
+
+	addrs = json_object_get(result, "addresses");
+	if (addrs == NULL) {
+		json_decref(result);
+		return (NS_UNAVAIL);
+	}
+
+	aliases = json_object_get(result, "aliases");
+	if (aliases == NULL) {
+		json_decref(result);
+		return (NS_UNAVAIL);
+	}
+
+	hp->h_addr_list = alloc_blob(&buf, &buflen,
+	    (json_array_size(addrs) + 1) * sizeof(void *));
+	hp->h_name = alloc_string(&buf, &buflen, json_string_value(
+	    json_object_get(result, "name")));
+	hp->h_length = sizeof(in_addr_t);
+	hp->h_addrtype = AF_INET;
+	hp->h_aliases = alloc_blob(&buf, &buflen,
+	    (json_array_size(aliases) + 1) * sizeof(void *));
+
+	json_array_foreach(addrs, idx, i) {
+		inet_aton(json_string_value(i), &addr);
+		hp->h_addr_list[idx] = alloc_blob(&buf, &buflen, sizeof(in_addr_t));
+		memcpy(hp->h_addr_list[idx], &addr, sizeof(in_addr_t));
+	}
+
+	hp->h_addr_list[idx] = NULL;
+
+	json_array_foreach(aliases, idx, i) {
+		hp->h_aliases[idx] = alloc_string(&buf, &buflen,
+		    json_string_value(i));
+	}
+
+	hp->h_aliases[idx] = NULL;
+
+	*resultp = hp;
+	*h_errnop = 0;
+	return (NS_SUCCESS);
 }
 
 int
