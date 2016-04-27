60e92693c13d764b8cf140bda28ad8df858d8eaa
Wojciech Kloska <shangteus@ixsystems.com>
2016-04-27 13:00:23 +0200

    REMOVED: unused import

--- src/dispatcher/plugins/ReplicationTransportPlugin.pyx
+++ src/dispatcher/plugins/ReplicationTransportPlugin.pyx
@@ -37,7 +37,7 @@ from freenas.utils import first_or_default
 from freenas.dispatcher.client import Client
 from freenas.dispatcher.fd import FileDescriptor
 from paramiko import AuthenticationException
-from freenas.dispatcher.rpc import RpcException, SchemaHelper as h, description, accepts, returns, private
+from freenas.dispatcher.rpc import RpcException, SchemaHelper as h, description, accepts, private
 from utils import get_replication_client
 from task import Task, ProgressTask, Provider, TaskException, TaskWarning, VerifyException, query
 from libc.stdlib cimport malloc, free
