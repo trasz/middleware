799ea258ba26ef50df45c75649bb7775e13d312b
Jakub Klama <jakub@ixsystems.com>
2016-05-23 02:11:09 +0200

    Remove dead connection from the list in force-logout case.

--- src/dispatcher/src/main.py
+++ src/dispatcher/src/main.py
@@ -1512,6 +1512,7 @@ class ServerConnection(WebSocketApplication, EventEmitter):
             # the reconnect will just log the session back in
             self.dispatcher.token_store.revoke_token(self.token)
             self.ws.close()
+            self.server.connections.pop(self, None)
         except WebSocketError as werr:
             # This error usually implies that the socket is dead
             # so just log it and move on
