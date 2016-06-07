cf5890213bd9839d85f4987e0b2090c22f2432ab
Jakub Klama <jakub@ixsystems.com>
2016-06-07 02:57:30 +0200

    Add migrations to delete system users and groups from the database.

---
+++ src/datastore/share/migrations/groups/0002-remove-system-groups.py
@@ -0,0 +1,42 @@
+#+
+# Copyright 2016 iXsystems, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#####################################################################
+
+
+TO_BE_REMOVED = [
+    'daemon', 'kmem', 'sys', 'tty', 'operator', 'mail', 'bin', 'news', 'man', 'games',
+    'ftp', 'staff', 'sshd', 'smmsp', 'mailnull', 'guest', 'proxy', 'bind', 'unbound',
+    'proxy', 'authpf', '_pflogd', '_dhcp', 'uucp', 'dialer', 'network', 'audit',
+    'www', 'ladvd', 'messagebus', 'webdav', 'mongodb', 'nogroup', 'nobody'
+]
+
+
+def probe(obj, ds):
+    return obj['name'] in TO_BE_REMOVED
+
+
+def apply(obj, ds):
+    return None
