2eed13f08610d8adf0279dfe00f55b1c19e497ca
Wojciech Kloska <shangteus@ixsystems.com>
2016-07-08 17:53:15 +0200

    FIXED: typo

--- src/dispatcher/plugins/VMPlugin.py
+++ src/dispatcher/plugins/VMPlugin.py
@@ -475,7 +475,7 @@ class VMImportTask(VMBaseTask):
                 'vm-{0}'.format(name)
             )
         except FileNotFoundError:
-            raise TaskException(errno.ENOENT, 'There is no {0} on {1} volume to be imported.'. format(name, volume))
+            raise TaskException(errno.ENOENT, 'There is no {0} on {1} volume to be imported.'.format(name, volume))
         except ValueError:
             raise VerifyException(
                 errno.EINVAL,
