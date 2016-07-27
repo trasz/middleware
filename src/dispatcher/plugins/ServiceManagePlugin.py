937c093cdc695f8bf700b0c008edc594044e4d94
Suraj Ravichandran <suraj@ixsystems.com>
2016-07-26 18:37:45 -0700

    Respect `filter` argument when spawing service query gevent threads.
    This makes it so that only the service's queried for are checked fori
    their status via expensive subprocess calls to rc. Thus, making
    the service namespace a bit faster in the cli.

--- src/dispatcher/plugins/ServiceManagePlugin.py
+++ src/dispatcher/plugins/ServiceManagePlugin.py
@@ -67,7 +67,7 @@ class ServiceInfoProvider(Provider):
 
         # Running extend sequentially might take too long due to the number of services
         # and `service ${name} onestatus`. To workaround that run it in parallel using gevent
-        result = self.datastore.query('service_definitions')
+        result = self.datastore.query('service_definitions', *(filter or []))
         if result is None:
             return result
         jobs = {
