4f5865080d35846f06b240c8c709c258ba0f43cc
William Grzybowski <wg@FreeBSD.org>
2016-08-02 13:44:49 -0300

    Add main script to run the tests

---
+++ src/restd/tests/main.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+def main():
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+    testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
+
+if __name__ == '__main__':
+    main()
