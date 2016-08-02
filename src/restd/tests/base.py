bfdf511d8c6da75f1debbe86a291ab834800fa60
William Grzybowski <wg@FreeBSD.org>
2016-08-02 14:46:16 -0300

    Add abstract test case and remove it after discover

--- src/restd/tests/base.py
+++ src/restd/tests/base.py
@@ -8,3 +8,12 @@ class RESTTestCase(unittest.TestCase):
 
     def setUp(self):
         self.client = Client(os.environ['URI'], '/api/v2.0/')
+
+
+class CRUDTestCase(RESTTestCase):
+
+    name = None
+
+    def test_list(self):
+        r = self.client.get(self.name)
+        self.assertTrue(isinstance(r, list))
--- src/restd/tests/main.py
+++ src/restd/tests/main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 import unittest
 
+from base import CRUDTestCase
+
+
+def remove_abstract_tests(tests):
+    if isinstance(tests, unittest.TestCase):
+        return tests
+    rv = []
+    for test in tests._tests:
+        # Skip CRUDTestCase
+        if test.__class__ is CRUDTestCase:
+            continue
+        rv.append(remove_abstract_tests(test))
+    tests._tests = rv
+    return tests
+
 
 def main():
     loader = unittest.TestLoader()
-    tests = loader.discover('.')
+    tests = loader.discover('resources')
+    tests = remove_abstract_tests(tests)
+
     testRunner = unittest.runner.TextTestRunner()
     testRunner.run(tests)
 
