5489da1a0797759ede1117668449eea8a886432c
William Grzybowski <wg@FreeBSD.org>
2016-08-02 17:48:29 -0300

    Add test_create to CRUD test case

--- src/restd/tests/base.py
+++ src/restd/tests/base.py
@@ -14,6 +14,15 @@ class CRUDTestCase(RESTTestCase):
 
     name = None
 
+    def get_create_data(self):
+        raise NotImplementedError('get_create_data needs to be implemented')
+
+    def test_create(self):
+        r = self.client.post(self.name, self.get_create_data())
+        self.assertEqual(r.status_code, 201)
+        data = r.json()
+        return r
+
     def test_retrieve(self):
         r = self.client.get(self.name)
         self.assertEqual(r.status_code, 200)
