2632091369576337f2db7bd07fba74d2003fb8e5
William Grzybowski <wg@FreeBSD.org>
2016-08-02 16:48:52 -0300

    Check for pool name

--- src/restd/tests/resources/test_boot.py
+++ src/restd/tests/resources/test_boot.py
@@ -4,6 +4,11 @@ from base import CRUDTestCase, SingleItemTestCase
 class BootPoolTestCase(SingleItemTestCase):
     name = 'boot/pool'
 
+    def test_retrieve(self):
+        r = super(BootPoolTestCase, self).test_retrieve()
+        data = r.json()
+        self.assertEqual(data['name'], 'freenas-boot')
+
 
 class BootEnvironmentTestCase(CRUDTestCase):
     name = 'boot/environment'
