0ec4b408a05bb51492e5dc14b44d3af3e011c763
William Grzybowski <wg@FreeBSD.org>
2016-08-03 15:05:12 -0300

    Allow tests to be skipped

--- src/restd/tests/main.py
+++ src/restd/tests/main.py
@@ -6,7 +6,7 @@ import unittest
 from base import CRUDTestCase, SingleItemTestCase
 
 
-def remove_abstract_tests(tests):
+def filter_tests(tests, skip=None):
     if isinstance(tests, unittest.TestCase):
         return tests
     rv = []
@@ -14,7 +14,10 @@ def remove_abstract_tests(tests):
         # Skip abstract test cases
         if test.__class__ in (CRUDTestCase, SingleItemTestCase):
             continue
-        rv.append(remove_abstract_tests(test))
+        if isinstance(test, unittest.TestCase):
+            if skip and test.__module__ in skip:
+                continue
+        rv.append(filter_tests(test, skip=skip))
     tests._tests = rv
     return tests
 
@@ -24,13 +27,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--uri', required=True)
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-s', '--skip', action='append')
     args = parser.parse_args()
 
     os.environ.setdefault('URI', args.uri)
 
     loader = unittest.TestLoader()
     tests = loader.discover('resources')
-    tests = remove_abstract_tests(tests)
+    tests = filter_tests(tests, skip=args.skip)
 
     testRunner = unittest.runner.TextTestRunner(verbosity=2 if args.verbose else 1)
     testRunner.run(tests)
