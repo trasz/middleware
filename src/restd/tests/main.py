4a2d4c5ab36c92472bbf3cfee7af6bfdcfe8a3c1
William Grzybowski <wg@FreeBSD.org>
2016-08-03 15:21:46 -0300

    Allow to execute only a certain set of tests

--- src/restd/tests/main.py
+++ src/restd/tests/main.py
@@ -6,7 +6,7 @@ import unittest
 from base import CRUDTestCase, SingleItemTestCase
 
 
-def filter_tests(tests, skip=None):
+def filter_tests(tests, only=None, skip=None):
     if isinstance(tests, unittest.TestCase):
         return tests
     rv = []
@@ -15,9 +15,11 @@ def filter_tests(tests, skip=None):
         if test.__class__ in (CRUDTestCase, SingleItemTestCase):
             continue
         if isinstance(test, unittest.TestCase):
+            if only and test.__module__ not in only:
+                continue
             if skip and test.__module__ in skip:
                 continue
-        rv.append(filter_tests(test, skip=skip))
+        rv.append(filter_tests(test, only=only, skip=skip))
     tests._tests = rv
     return tests
 
@@ -28,13 +30,14 @@ def main():
     parser.add_argument('-u', '--uri', required=True)
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-s', '--skip', action='append')
+    parser.add_argument('-t', '--test', action='append')
     args = parser.parse_args()
 
     os.environ.setdefault('URI', args.uri)
 
     loader = unittest.TestLoader()
     tests = loader.discover('resources')
-    tests = filter_tests(tests, skip=args.skip)
+    tests = filter_tests(tests, only=args.test, skip=args.skip)
 
     testRunner = unittest.runner.TextTestRunner(verbosity=2 if args.verbose else 1)
     testRunner.run(tests)
