c4b5123f619fd5fd8002fd2c42f1df778e37852e
William Grzybowski <wg@FreeBSD.org>
2016-08-03 15:25:49 -0300

    Allow unit test classes to be selectively skipped

--- src/restd/tests/main.py
+++ src/restd/tests/main.py
@@ -6,7 +6,7 @@ import unittest
 from base import CRUDTestCase, SingleItemTestCase
 
 
-def filter_tests(tests, only=None, skip=None):
+def filter_tests(tests, only=None, skip=None, skip_class=None):
     if isinstance(tests, unittest.TestCase):
         return tests
     rv = []
@@ -19,7 +19,9 @@ def filter_tests(tests, only=None, skip=None):
                 continue
             if skip and test.__module__ in skip:
                 continue
-        rv.append(filter_tests(test, only=only, skip=skip))
+            if test.__class__.__name__ in skip_class:
+                continue
+        rv.append(filter_tests(test, only=only, skip=skip, skip_class=skip_class))
     tests._tests = rv
     return tests
 
@@ -30,6 +32,7 @@ def main():
     parser.add_argument('-u', '--uri', required=True)
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-s', '--skip', action='append')
+    parser.add_argument('-sc', '--skip-class', action='append')
     parser.add_argument('-t', '--test', action='append')
     args = parser.parse_args()
 
@@ -37,7 +40,7 @@ def main():
 
     loader = unittest.TestLoader()
     tests = loader.discover('resources')
-    tests = filter_tests(tests, only=args.test, skip=args.skip)
+    tests = filter_tests(tests, only=args.test, skip=args.skip, skip_class=args.skip_class)
 
     testRunner = unittest.runner.TextTestRunner(verbosity=2 if args.verbose else 1)
     testRunner.run(tests)
