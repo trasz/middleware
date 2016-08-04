1e49ba9139f06b5054f46c8a02c81765993339bc
William Grzybowski <wg@FreeBSD.org>
2016-08-04 11:46:16 -0300

    Add extra args necessary for VLAN and renew test

--- src/restd/tests/resources/test_network_interface.py
+++ src/restd/tests/resources/test_network_interface.py
@@ -8,6 +8,11 @@ class NetworkInterfaceTestCase(CRUDTestCase):
         return {
             'type': 'VLAN',
             'id': 'vlan0',
+            'dhcp': True,
+            'vlan': {
+                'parent': 'em0',
+                'tag': 10,
+            }
         }
 
     def test_041_down(self):
@@ -16,6 +21,10 @@ class NetworkInterfaceTestCase(CRUDTestCase):
 
     def test_042_up(self):
         r = self.client.post(self.name + '/vlan0/up')
+        self.assertEqual(r.status_code, 201, msg=r.text)
+
+    def test_043_renew(self):
+        r = self.client.post(self.name + '/vlan0/renew')
         self.assertEqual(r.status_code, 201, msg=r.text)
 
     def get_delete_identifier(self):
