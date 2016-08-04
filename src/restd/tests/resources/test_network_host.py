c22c62dd2dd39a64cf3db60a3d436ba50eb7256a
William Grzybowski <wg@FreeBSD.org>
2016-08-04 13:46:45 -0300

    Update test for network host

--- src/restd/tests/resources/test_network_host.py
+++ src/restd/tests/resources/test_network_host.py
@@ -10,5 +10,10 @@ class NetworkHostTestCase(CRUDTestCase):
             'addresses': ['1.1.1.1', '2.2.2.2'],
         }
 
+    def get_update_ident_data(self):
+        return 'freenas.test', {
+            'addresses': ['1.1.1.1'],
+        }
+
     def get_delete_identifier(self):
         return 'freenas.test'
