2bb5a0a7bc34e90611f204637c6e1815a188371a
William Grzybowski <wg@FreeBSD.org>
2016-08-04 16:10:02 -0300

    Expose share namespace, plain and simple
    Goal is to be as close as WebSocket API as possible.

--- src/restd/plugins/share.py
+++ src/restd/plugins/share.py
@@ -1,27 +1,9 @@
-from base import CRUDBase, EntityResource
+from base import CRUDBase
 
 
-class ShareSMBEntityResource(EntityResource):
-
-    def run_get(self, req, urlparams):
-        """Always inject a type=smb filter to share.query"""
-        args, kwargs = super(ShareSMBEntityResource, self).run_get(req, urlparams)
-        args[0].append(('type', '=', 'smb'))
-        return args, kwargs
-
-    def run_post(self, req, urlparams):
-        data = req.context['doc']
-        data['type'] = 'smb'
-        if 'properties' in data:
-            data['properties']['type'] = 'service-smb'
-        return [data]
-
-
-class ShareSMBResource(CRUDBase):
-    name = 'share/smb'
+class ShareResource(CRUDBase):
     namespace = 'share'
-    entity_class = ShareSMBEntityResource
 
 
 def _init(rest):
-    rest.register_crud(ShareSMBResource)
+    rest.register_crud(ShareResource)
