7f4ea591178ac70fed3ff6ce4fbf3729445705ab
William Grzybowski <wg@FreeBSD.org>
2016-08-05 11:01:32 -0300

    Update FTP test for NWO

--- src/restd/tests/resources/test_service_ftp.py
+++ src/restd/tests/resources/test_service_ftp.py
@@ -1,46 +1,68 @@
-from base import SingleItemTestCase
+from base import RESTTestCase
 
 
-class ServiceFTPTestCase(SingleItemTestCase):
-    name = 'service/ftp'
+class ServiceFTPTestCase(RESTTestCase):
 
-    def get_update_data(self):
-        return {
-            'reverse_dns': True,
-            'root_login': True,
-            'ident': True,
-            'filemask': {
-                'group': {
-                    'read': True,
-                    'execute': True,
-                    'write': True
+    def test_020_retrieve(self):
+        r = self.client.get('service', params={
+            'name': 'ftp',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+
+    def test_040_update(self):
+        r = self.client.get('service', params={
+            'name': 'ftp',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        id = data[0]['id']
+        r = self.client.put('service', data=[
+            id,
+            {
+                'config': {
+                    'reverse_dns': True,
+                    'root_login': True,
+                    'ident': True,
+                    'filemask': {
+                        'group': {
+                            'read': True,
+                            'execute': True,
+                            'write': True
+                        },
+                        'others': {
+                            'read': True,
+                            'execute': True,
+                            'write': True
+                        },
+                        'user': {
+                            'read': False,
+                            'execute': False,
+                            'write': False
+                        }
+                    },
+                    'dirmask': {
+                        'group': {
+                            'read': True,
+                            'execute': True,
+                            'write': True
+                        },
+                        'others': {
+                            'read': True,
+                            'execute': True,
+                            'write': True
+                        },
+                        'user': {
+                            'read': False,
+                            'execute': False,
+                            'write': False
+                        }
+                    },
                 },
-                'others': {
-                    'read': True,
-                    'execute': True,
-                    'write': True
-                },
-                'user': {
-                    'read': False,
-                    'execute': False,
-                    'write': False
-                }
             },
-            'dirmask': {
-                'group': {
-                    'read': True,
-                    'execute': True,
-                    'write': True
-                },
-                'others': {
-                    'read': True,
-                    'execute': True,
-                    'write': True
-                },
-                'user': {
-                    'read': False,
-                    'execute': False,
-                    'write': False
-                }
-            },
-        }
+        ])
+        self.assertEqual(r.status_code, 200, msg=r.text)
