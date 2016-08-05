93dc5909242dc5eeae2f62a2ea50ae410284c9cd
William Grzybowski <wg@FreeBSD.org>
2016-08-05 10:50:08 -0300

    Fix afp service test following service plugin change

--- src/restd/tests/resources/test_service_afp.py
+++ src/restd/tests/resources/test_service_afp.py
@@ -1,11 +1,32 @@
-from base import SingleItemTestCase
+from base import RESTTestCase
 
 
-class ServiceAFPTestCase(SingleItemTestCase):
-    name = 'service/afp'
+class ServiceAFPTestCase(RESTTestCase):
 
-    def get_update_data(self):
-        return {
-            'guest_enable': True,
-            'connections_limit': 100,
-        }
+    def test_020_retrieve(self):
+        r = self.client.get('service', params={
+            'name': 'afp',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+
+    def test_040_update(self):
+        r = self.client.get('service', params={
+            'name': 'afp',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        id = data[0]['id']
+        r = self.client.put('service', data=[
+            id,
+            {
+                'config': {
+                    'connections_limit': 100,
+                },
+            },
+        ])
+        self.assertEqual(r.status_code, 200, msg=r.text)
