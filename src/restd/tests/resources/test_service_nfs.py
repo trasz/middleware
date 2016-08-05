c9a2b3b56a3b0afd018ec15763427e8b1dc9946e
William Grzybowski <wg@FreeBSD.org>
2016-08-05 11:02:23 -0300

    Update NFS test for NWO

--- src/restd/tests/resources/test_service_nfs.py
+++ src/restd/tests/resources/test_service_nfs.py
@@ -1,11 +1,33 @@
-from base import SingleItemTestCase
+from base import RESTTestCase
 
 
-class ServiceNFSTestCase(SingleItemTestCase):
-    name = 'service/nfs'
+class ServiceNFSTestCase(RESTTestCase):
 
-    def get_update_data(self):
-        return {
-            'v4': True,
-            'servers': 8,
-        }
+    def test_020_retrieve(self):
+        r = self.client.get('service', params={
+            'name': 'nfs',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+
+    def test_040_update(self):
+        r = self.client.get('service', params={
+            'name': 'nfs',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        id = data[0]['id']
+        r = self.client.put('service', data=[
+            id,
+            {
+                'config': {
+                    'v4': True,
+                    'servers': 8,
+                },
+            },
+        ])
+        self.assertEqual(r.status_code, 200, msg=r.text)
