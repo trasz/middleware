8d7f68865a795188b9ce25b145ae2d266c1c9edc
William Grzybowski <wg@FreeBSD.org>
2016-08-05 10:56:58 -0300

    Update DynDNS test for NWO

--- src/restd/tests/resources/test_service_dyndns.py
+++ src/restd/tests/resources/test_service_dyndns.py
@@ -1,11 +1,33 @@
-from base import SingleItemTestCase
+from base import RESTTestCase
 
 
-class ServiceDynDNSTestCase(SingleItemTestCase):
-    name = 'service/dyndns'
+class ServiceDynDNSTestCase(RESTTestCase):
 
-    def get_update_data(self):
-        return {
-            'provider': 'dyndns@dyndns.org',
-            'domains': ['apitest.dyndns.org'],
-        }
+    def test_020_retrieve(self):
+        r = self.client.get('service', params={
+            'name': 'dyndns',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+
+    def test_040_update(self):
+        r = self.client.get('service', params={
+            'name': 'dyndns',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        id = data[0]['id']
+        r = self.client.put('service', data=[
+            id,
+            {
+                'config': {
+                    'provider': 'dyndns@dyndns.org',
+                    'domains': ['apitest.dyndns.org'],
+                },
+            },
+        ])
+        self.assertEqual(r.status_code, 200, msg=r.text)
