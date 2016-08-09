c8ebf828bb8d4fe6fc0abd02e1335383092a8913
William Grzybowski <wg@FreeBSD.org>
2016-08-09 10:36:32 -0300

    feat(restd): add system.advanced resource

--- src/restd/plugins/system.py
+++ src/restd/plugins/system.py
@@ -1,9 +1,14 @@
 from base import SingleItemBase
 
 
+class SystemAdvancedSingleItem(SingleItemBase):
+    namespace = 'system.advanced'
+
+
 class SystemGeneralSingleItem(SingleItemBase):
     namespace = 'system.general'
 
 
 def _init(rest):
+    rest.register_singleitem(SystemAdvancedSingleItem)
     rest.register_singleitem(SystemGeneralSingleItem)
