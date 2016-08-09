a7d06cf2fd0db3d5cd5cfa07480aba9f736d6ff0
William Grzybowski <wg@FreeBSD.org>
2016-08-09 13:37:41 -0300

    feat(restd): add filesystem plugin

---
+++ src/restd/plugins/filesystem.py
@@ -0,0 +1,23 @@
+from base import Resource
+
+
+class FilesystemListDirResource(Resource):
+    name = 'filesystem/list_dir'
+    post = 'rpc:filesystem.list_dir'
+
+
+class FilesystemStatResource(Resource):
+    name = 'filesystem/stat'
+    post = 'rpc:filesystem.stat'
+
+
+class FilesystemGetOpenFilesResource(Resource):
+    name = 'filesystem/get_open_files'
+    post = 'rpc:filesystem.get_open_files'
+
+
+def _init(rest):
+    # TODO: filesystem.{download,upload}
+    rest.register_resource(FilesystemListDirResource)
+    rest.register_resource(FilesystemStatResource)
+    rest.register_resource(FilesystemGetOpenFilesResource)
