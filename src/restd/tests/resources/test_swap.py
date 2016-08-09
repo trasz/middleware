9491c6a94d47850863d15b04798501cbd8429240
William Grzybowski <wg@FreeBSD.org>
2016-08-09 12:09:10 -0300

    feat(restd): swap info test

---
+++ src/restd/tests/resources/test_swap.py
@@ -0,0 +1,11 @@
+from base import RESTTestCase
+
+
+class SwapTestCase(RESTTestCase):
+    name = 'swap'
+
+    def test_020_info(self):
+        r = self.client.get(self.name + '/info')
+        self.assertEqual(r.status_code, 200)
+        data = r.json()
+        self.assertIsInstance(data, list)
