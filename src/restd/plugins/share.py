5ff2002e7dc3e26ec336fefbcba9a4b48585052f
William Grzybowski <wg@FreeBSD.org>
2016-08-09 16:13:20 -0300

    feat(restd): add iscsi resources

--- src/restd/plugins/share.py
+++ src/restd/plugins/share.py
@@ -5,5 +5,23 @@ class ShareResource(CRUDBase):
     namespace = 'share'
 
 
+class ShareISCSIAuthResource(CRUDBase):
+    name = 'share_iscsi/auth'  # FIXME: custom name until we figure out conflicts
+    namespace = 'share.iscsi.auth'
+
+
+class ShareISCSIPortalResource(CRUDBase):
+    name = 'share_iscsi/portal'  # FIXME: custom name until we figure out conflicts
+    namespace = 'share.iscsi.portal'
+
+
+class ShareISCSITargetResource(CRUDBase):
+    name = 'share_iscsi/target'  # FIXME: custom name until we figure out conflicts
+    namespace = 'share.iscsi.target'
+
+
 def _init(rest):
     rest.register_crud(ShareResource)
+    rest.register_crud(ShareISCSIAuthResource)
+    rest.register_crud(ShareISCSIPortalResource)
+    rest.register_crud(ShareISCSITargetResource)
