bca437e80a91b5821c0ae7e08ed2a9f09ebeb4a8
William Grzybowski <wg@FreeBSD.org>
2016-08-09 11:38:00 -0300

    feat(restd): add system info rpc methods

--- src/restd/plugins/system.py
+++ src/restd/plugins/system.py
@@ -1,4 +1,4 @@
-from base import SingleItemBase
+from base import Resource, SingleItemBase
 
 
 class SystemAdvancedSingleItem(SingleItemBase):
@@ -17,8 +17,32 @@ class SystemUISingleItem(SingleItemBase):
     namespace = 'system.ui'
 
 
+class SystemInfoUnameFullResource(Resource):
+    name = 'system/info/uname_full'
+    get = 'rpc:system.info.uname_full'
+
+
+class SystemInfoVersionResource(Resource):
+    name = 'system/info/version'
+    get = 'rpc:system.info.version'
+
+
+class SystemInfoLoadAvgResource(Resource):
+    name = 'system/info/load_avg'
+    get = 'rpc:system.info.load_avg'
+
+
+class SystemInfoHardwareResource(Resource):
+    name = 'system/info/hardware'
+    get = 'rpc:system.info.hardware'
+
+
 def _init(rest):
     rest.register_singleitem(SystemAdvancedSingleItem)
     rest.register_singleitem(SystemGeneralSingleItem)
     rest.register_singleitem(SystemTimeSingleItem)
     rest.register_singleitem(SystemUISingleItem)
+    rest.register_resource(SystemInfoUnameFullResource)
+    rest.register_resource(SystemInfoVersionResource)
+    rest.register_resource(SystemInfoLoadAvgResource)
+    rest.register_resource(SystemInfoHardwareResource)
