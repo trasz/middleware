02bafce9bb0eaee6f75229c6a5c5baf4e42cda49
Jakub Klama <jakub@ixsystems.com>
2016-08-10 03:19:03 +0200

    debugd initial iteration: implemented connecting to debug proxy server, shell proxying and process management.

--- src/debugd/src/main.py
+++ src/debugd/src/main.py
@@ -28,50 +28,159 @@
 import os
 import pty
 import signal
+import enum
+import uuid
 import setproctitle
 import time
 import threading
 import logging
 import argparse
+import select
+import socket
+import stat
+import bsd
 import msock.channel
 import msock.client
-from freenas.dispatcher.rpc import RpcContext
+from datetime import datetime
+from threading import Condition
+from freenas.dispatcher.rpc import RpcContext, RpcService, generator
+from freenas.dispatcher.fd import MSockChannelSerializer
+from freenas.dispatcher.bridge import Bridge
+from freenas.dispatcher.client import Client
 from freenas.dispatcher.server import Server
+from freenas.utils.permissions import get_type
 
 
-SUPPORT_PROXY_ADDRESS = 'tcp://127.0.0.1:5678'  # XXX
+CORES_DIR = '/var/db/system/cores'
+SUPPORT_PROXY_ADDRESS = 'tcp://10.20.0.32:8080'  # XXX
 
 
-class ShellChannel(msock.channel.Channel):
-    def __init__(self, connection, id, metadata):
-        super(ShellChannel, self).__init__(connection, id, metadata)
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.pid, self.master = pty.fork()
+class ConnectionState(enum.Enum):
+    OFFLINE = 'offline'
+    CONNECTING = 'connecting'
+    CONNECTED = 'connected'
 
-        if self.pid == 0:
-            os.execv(metadata['path'], metadata['args'])
 
-        self.logger.info('Spawned {0} as pid {1}'.format(metadata['path'], self.pid))
-        self.reader_thread = threading.Thread(target=self.reader, daemon=True, name='shell reader thread')
-        self.reader_thread.start()
+class DebugService(RpcService):
+    def __init__(self, context):
+        self.context = context
 
-    def on_data(self, data):
-        os.write(self.master, data)
+    def shell(self, args, fd):
+        self.context.jobs.append(ShellConnection(args, fd))
+
+    def download(self, path, fd):
+        pass
+
+    def upload(self, path, fd):
+        pass
+
+    @generator
+    def listdir(self, directory):
+        for i in os.listdir(directory):
+            path = os.path.join(directory, i)
+            stat = os.lstat(path)
+            yield {
+                'type': get_type(stat),
+                'size': stat.st_size,
+                'created_at': None,
+                'modified_at': None,
+                'name': i
+            }
+
+    @generator
+    def get_process_list(self):
+        procs =  bsd.getprocs(bsd.ProcessLookupPredicate.PROC)
+        for i in procs:
+            try:
+                yield {
+                    'pid': i.pid,
+                    'command': i.command,
+                    'argv': list(i.argv),
+                    'env': list(i.env)
+                }
+            except:
+                continue
+
+    @generator
+    def get_core_files(self):
+        for i in os.listdir(CORES_DIR):
+            try:
+                path = os.path.join(CORES_DIR, i)
+                proc = bsd.opencore(path)
+                stat = os.stat(path)
+                yield {
+                    'pid': p.pid,
+                    'command': p.command,
+                    'argv': p.argv,
+                    'created_at': stat.st,
+                    'size': stat.st_size
+                }
+            except OSError:
+                continue
+
+
+    def trace_process(self, pid, fd):
+        self.context.jobs.append(ShellConnection(['/usr/bin/truss', '-p', str(pid)], fd))
+
+    def proxy_dispatcher_rpc(self, fd):
+        br = Bridge()
+        br.start('unix:', 'fd://{0}'.format(fd.fd))
+
+
+class ControlService(RpcService):
+    def __init__(self, context):
+        self.context = context
+
+    def connect(self, discard=False):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def status(self):
+        return {
+            'state': self.context.state,
+            'server': None,
+            'connected_at': None,
+            'jobs': [j.__getstate__() for j in self.context.jobs]
+        }
+
+    def upload(self, filename, description, fd):
+        pass
+
+
+class ShellConnection(object):
+    def __init__(self, path, fd):
+        try:
+            self.logger = logging.getLogger(self.__class__.__name__)
+            self.logger.info('Attempting to start shell session of {0}'.format(path))
+            self.pid, self.master = pty.fork()
+            self.fd = fd.fd
+
+            if self.pid == 0:
+                os.execv(path[0], path)
+
+            self.logger.info('Spawned {0} as pid {1}'.format(path, self.pid))
+            self.reader_thread = threading.Thread(target=self.reader, daemon=True, name='shell reader thread')
+            self.reader_thread.start()
+        except:
+            self.logger.exception('exc')
 
     def reader(self):
         while True:
-            data = os.read(self.master, 1024)
-            if data == b'':
-                self.logger.debug('Closing shell session')
-                self.close()
-                return
+            r, _, _ = select.select([self.fd, self.master], [], [])
+            for fd in r:
+                data = os.read(fd, 1024)
+                if data == b'':
+                    self.logger.debug('Closing shell session')
+                    os.close(self.fd if fd == self.master else self.master)
+                    return
 
-            self.write(data)
+                os.write(self.fd if fd == self.master else self.master, data)
 
 
-class FileTransferChannel(msock.channel.Channel):
-    def __init__(self, connection, id, metadata):
-        super(FileTransferChannel, self).__init__(connection, id, metadata)
+class FileTransfer(object):
+    def __init__(self, path, fd):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.upload = metadata['direction']
         self.file = open(metadata['path'])
@@ -97,56 +206,50 @@ class FileTransferChannel(msock.channel.Channel):
             self.write(data)
 
 
-class ControlRPCChannel(msock.channel.Channel):
-    def __init__(self, context, connection, id, metadata):
-        super(ControlRPCChannel, self).__init__(connection, id, metadata)
-        self.context = connection
-
-    def on_data(self, data):
-        pass
-
-
-class DispatcherRPCChannel(msock.channel.Channel):
-    pass
-
-
 class Context(object):
     def __init__(self):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.msock = msock.client.Client()
-        self.msock.on_channel_created = self.on_channel_created
-        self.msock.on_channel_destroyed = self.on_channel_destroyed
-        self.msock.channel_factory = self.channel_factory
+        self.rpc_fd = -1
+        self.connection_id = None
+        self.jobs = []
+        self.state = ConnectionState.OFFLINE
+        self.cv = Condition()
         self.rpc = RpcContext()
-        self.server = Server(self)
+        self.client = Client()
+        self.server = Server()
 
     def start(self):
         signal.signal(signal.SIGUSR2, lambda signo, frame: self.connect())
+        self.server.rpc = RpcContext()
+        self.server.rpc.register_service_instance('control', ControlService(self))
+        self.server.start('unix:///var/run/debugd.sock')
+        threading.Thread(target=self.server.serve_forever).start()
 
-    def channel_factory(self, id, metadata):
-        if metadata['type'] == 'shell':
-            return ShellChannel(self.msock, id, metadata)
+    def connect(self, discard=False):
+        if discard:
+            self.connection_id = None
 
-        if metadata['type'] == 'file':
-            return FileTransferChannel(self.msock, id, metadata)
-
-        if metadata['type'] == 'dispatcher':
-            return DispatcherRPCChannel(self.msock, id, metadata)
-
-        if metadata['type'] == 'rpc':
-            return ControlRPCChannel(self, self.msock, id, metadata)
-
-        raise ValueError('Unknown channel type')
-
-    def on_channel_created(self, chan):
-        pass
-
-    def on_channel_destroyed(self, chan):
-        pass
-
-    def connect(self):
+        self.set_state(ConnectionState.CONNECTING)
         self.msock.connect(SUPPORT_PROXY_ADDRESS)
         self.logger.info('Connecting to {0}'.format(SUPPORT_PROXY_ADDRESS))
+        self.connection_id = uuid.uuid4()
+        self.rpc_fd = self.msock.create_channel(0).fileno()
+        time.sleep(1)  # FIXME
+        self.client.connect('fd://{0}'.format(self.rpc_fd))
+        self.client.channel_serializer = MSockChannelSerializer(self.msock)
+        self.client.standalone_server = True
+        self.client.enable_server()
+        self.client.register_service('debug', DebugService(self))
+        self.client.call_sync('server.login', str(self.connection_id), socket.gethostname(), 'none')
+        self.set_state(ConnectionState.CONNECTED)
+
+    def connect_worker(self):
+        while True:
+            pass
+
+    def set_state(self, state):
+        self.state = state
 
 
 def main():
@@ -154,7 +257,12 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     context = Context()
     context.start()
+    context.connect()
     setproctitle.setproctitle('debugd')
+
+    if not os.path.isdir(CORES_DIR):
+        os.mkdir(CORES_DIR)
+
     while True:
         time.sleep(60)
 
