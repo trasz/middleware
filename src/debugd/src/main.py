d92ac56a8068ef3f84e2c4c6e247491d3f0dcb76
Jakub Klama <jakub@ixsystems.com>
2016-08-10 22:17:11 +0200

    Fix various issues, add file download/upload handler.

--- src/debugd/src/main.py
+++ src/debugd/src/main.py
@@ -37,7 +37,7 @@ import logging
 import argparse
 import select
 import socket
-import stat
+import tarfile
 import bsd
 import msock.channel
 import msock.client
@@ -52,6 +52,8 @@ from freenas.utils.permissions import get_type
 
 
 CORES_DIR = '/var/db/system/cores'
+DEFAULT_CONFIGFILE = '/usr/local/etc/debugd.conf'
+DEFAULT_SOCKET_ADDRESS = 'unix:///var/run/debugd.sock'
 SUPPORT_PROXY_ADDRESS = 'tcp://10.20.0.32:8080'  # XXX
 
 
@@ -66,13 +68,23 @@ class DebugService(RpcService):
         self.context = context
 
     def shell(self, args, fd):
-        self.context.jobs.append(ShellConnection(args, fd))
+        self.context.run_job(ShellConnection(args, fd))
 
     def download(self, path, fd):
-        pass
+        job = FileConnection(path, False, fd)
+        size = job.filesize
+        self.context.run_job(job)
+        return size
 
     def upload(self, path, fd):
-        pass
+        job = FileConnection(path, True, fd)
+        self.context.run_job(job)
+
+    def tail(self, path, scrollback_size, fd):
+        self.context.run_job(TailConnection(path, fd))
+
+    def dashboard(self, fd):
+        self.context.run_job(DashboardConnection(fd))
 
     @generator
     def listdir(self, directory):
@@ -82,14 +94,14 @@ class DebugService(RpcService):
             yield {
                 'type': get_type(stat),
                 'size': stat.st_size,
-                'created_at': None,
-                'modified_at': None,
+                'created_at': datetime.utcfromtimestamp(stat.st_ctime),
+                'modified_at': datetime.utcfromtimestamp(stat.st_mtime),
                 'name': i
             }
 
     @generator
     def get_process_list(self):
-        procs =  bsd.getprocs(bsd.ProcessLookupPredicate.PROC)
+        procs = bsd.getprocs(bsd.ProcessLookupPredicate.PROC)
         for i in procs:
             try:
                 yield {
@@ -109,15 +121,14 @@ class DebugService(RpcService):
                 proc = bsd.opencore(path)
                 stat = os.stat(path)
                 yield {
-                    'pid': p.pid,
-                    'command': p.command,
-                    'argv': p.argv,
-                    'created_at': stat.st,
+                    'pid': proc.pid,
+                    'command': proc.command,
+                    'argv': proc.argv,
+                    'created_at': datetime.utcfromtimestamp(stat.st_ctime),
                     'size': stat.st_size
                 }
             except OSError:
                 continue
-
 
     def trace_process(self, pid, fd):
         self.context.jobs.append(ShellConnection(['/usr/bin/truss', '-p', str(pid)], fd))
@@ -149,24 +160,50 @@ class ControlService(RpcService):
         pass
 
 
-class ShellConnection(object):
-    def __init__(self, path, fd):
+class Job(object):
+    def __init__(self):
+        self.context = None
+        self.created_at = datetime.now()
+
+    def describe(self):
+        raise NotImplementedError()
+
+    def close(self):
+        if self.context:
+            self.context.jobs.remove(self)
+
+    def __getstate__(self):
+        return {
+            'created_at': self.created_at,
+            'description': self.describe()
+        }
+
+
+class ShellConnection(Job):
+    def __init__(self, args, fd):
+        super(ShellConnection, self).__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info('Attempting to start shell session of {0}'.format(args))
+        self.args = args
+        self.fd = fd.fd
+
         try:
-            self.logger = logging.getLogger(self.__class__.__name__)
-            self.logger.info('Attempting to start shell session of {0}'.format(path))
             self.pid, self.master = pty.fork()
-            self.fd = fd.fd
 
             if self.pid == 0:
-                os.execv(path[0], path)
+                os.execv(args[0], args)
 
-            self.logger.info('Spawned {0} as pid {1}'.format(path, self.pid))
-            self.reader_thread = threading.Thread(target=self.reader, daemon=True, name='shell reader thread')
+            self.logger.info('Spawned {0} as pid {1}'.format(args, self.pid))
+            self.reader_thread = threading.Thread(target=self.worker, daemon=True, name='shell reader thread')
             self.reader_thread.start()
-        except:
-            self.logger.exception('exc')
+        except OSError as err:
+            self.logger.error('Cannot start shell connection: {0}'.format(str(err)))
+            self.close()
 
-    def reader(self):
+    def describe(self):
+        return "Shell: {0}".format(' '.join(self.args))
+
+    def worker(self):
         while True:
             r, _, _ = select.select([self.fd, self.master], [], [])
             for fd in r:
@@ -174,36 +211,60 @@ class ShellConnection(object):
                 if data == b'':
                     self.logger.debug('Closing shell session')
                     os.close(self.fd if fd == self.master else self.master)
+                    self.close()
                     return
 
                 os.write(self.fd if fd == self.master else self.master, data)
 
 
-class FileTransfer(object):
-    def __init__(self, path, fd):
+class FileConnection(Job):
+    def __init__(self, path, upload, fd):
+        super(FileConnection, self).__init__()
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.upload = metadata['direction']
-        self.file = open(metadata['path'])
-        if self.upload:
-            self.reader_thread = threading.Thread(target=self.reader, daemon=True, name='file reader thread')
-            self.reader_thread.start()
+        self.upload = upload
+        self.path = path
+        self.fd = fd.fd
+        self.file = open(path, 'rb' if upload else 'wb')
+        self.reader_thread = threading.Thread(target=self.worker, daemon=True, name='file reader thread')
+        self.reader_thread.start()
 
-    def on_data(self, data):
-        if not self.upload:
-            if not data:
-                self.file.close()
+    @property
+    def filesize(self):
+        stat = os.stat(self.path)
+        return stat.st_size
 
-            self.file.write(data)
+    def describe(self):
+        return "File {0}: {1}".format('upload' if self.upload else 'download', self.path)
 
-    def reader(self):
+    def worker(self):
         while True:
-            data = os.read(self.file.fileno(), 1024)
+            data = os.read(self.file.fileno() if self.upload else self.fd, 1024)
             if data == b'':
+                os.close(self.fd)
                 self.file.close()
                 self.close()
                 return
 
-            self.write(data)
+            os.write(self.fd if self.upload else self.file.fileno(), data)
+
+
+class TailConnection(Job):
+    def __init__(self, path, fd):
+        super(TailConnection, self).__init__()
+        self.path = path
+        self.fd = fd
+
+    def describe(self):
+        return "File watch: {0}".format(self.path)
+
+    def worker(self):
+        pass
+
+
+class DashboardConnection(Job):
+    def __init__(self, fd):
+        super(DashboardConnection, self).__init__()
+        self.fd = fd.fd
 
 
 class Context(object):
@@ -219,11 +280,11 @@ class Context(object):
         self.client = Client()
         self.server = Server()
 
-    def start(self):
+    def start(self, sockpath):
         signal.signal(signal.SIGUSR2, lambda signo, frame: self.connect())
         self.server.rpc = RpcContext()
         self.server.rpc.register_service_instance('control', ControlService(self))
-        self.server.start('unix:///var/run/debugd.sock')
+        self.server.start(sockpath)
         threading.Thread(target=self.server.serve_forever, name='server thread', daemon=True).start()
 
     def connect(self, discard=False):
@@ -244,19 +305,24 @@ class Context(object):
         self.client.call_sync('server.login', str(self.connection_id), socket.gethostname(), 'none')
         self.set_state(ConnectionState.CONNECTED)
 
-    def connect_worker(self):
-        while True:
-            pass
+    def run_job(self, job):
+        self.jobs.append(job)
+        job.context = self
+        job.start()
 
     def set_state(self, state):
-        self.state = state
+        with self.cv:
+            self.state = state
+            self.cv.notify_all()
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-s', metavar='SOCKET', default=DEFAULT_SOCKET_ADDRESS, help='Socket address to listen on')
+    args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG)
     context = Context()
-    context.start()
+    context.start(args.s)
     context.connect()
     setproctitle.setproctitle('debugd')
 
