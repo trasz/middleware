0535368a7bdb4e49232d8645e8b84bb0cd6da7d7
William Grzybowski <wg@FreeBSD.org>
2016-08-10 10:17:23 -0300

    feat(restd): show response body for filesystem test assertion

--- src/restd/tests/resources/test_filesystem.py
+++ src/restd/tests/resources/test_filesystem.py
@@ -6,19 +6,19 @@ class FilesystemTestCase(RESTTestCase):
 
     def test_020_list_dir(self):
         r = self.client.post(self.name + '/list_dir', data=['/'])
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, 201, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, list)
 
     def test_021_stat(self):
         r = self.client.post(self.name + '/stat', data=['/usr'])
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, 201, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, dict)
 
     def test_022_get_open_files(self):
         r = self.client.post(self.name + '/get_open_files', data=['/'])
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, 201, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, list)
 
@@ -32,5 +32,5 @@ class FileTestCase(RESTTestCase):
                 'user': 'daemon',
             }
         ])
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, 201, msg=r.text)
         data = r.json()
