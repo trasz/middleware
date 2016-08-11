1808e4394867b978447bf83690b1f4e6a418657f
William Grzybowski <wg@FreeBSD.org>
2016-08-11 14:43:06 -0300

    feat(restd): add update tasks to resource

--- src/restd/plugins/update.py
+++ src/restd/plugins/update.py
@@ -1,15 +1,45 @@
 from base import ProviderMixin, Resource, SingleItemBase
 
 
+class ApplyResource(Resource):
+    name = 'apply'
+    post = 'task:update.apply'
+
+
 class CheckResource(Resource):
     name = 'check'
     post = 'task:update.check'
 
 
+class CheckFetchResource(Resource):
+    name = 'checkfetch'
+    post = 'task:update.checkfetch'
+
+
+class DownloadResource(Resource):
+    name = 'download'
+    post = 'task:update.download'
+
+
+class UpdateNowResource(Resource):
+    name = 'updatenow'
+    post = 'task:update.updatenow'
+
+
+class VerifyResource(Resource):
+    name = 'verify'
+    post = 'task:update.verify'
+
+
 class UpdateSingleItem(SingleItemBase):
     namespace = 'update'
     subresources = (
+        ApplyResource,
         CheckResource,
+        CheckFetchResource
+        DownloadResource,
+        UpdateNowResource,
+        VerifyResource,
     )
 
 
