e4f8942952af7257e4dc1597acd9ee18c1eea26d
William Grzybowski <wg@FreeBSD.org>
2016-08-11 11:04:10 -0300

    feat(restd): add update tests

---
+++ src/restd/tests/resources/test_update.py
@@ -0,0 +1,54 @@
+from base import SingleItemTestCase
+
+
+class UpdateTestCase(SingleItemTestCase):
+    name = 'update'
+
+    def get_update_data(self):
+        return {
+            'check_auto': False,
+        }
+
+    def test_071_is_update_available(self):
+        r = self.client.get(self.name + '/is_update_available')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, bool)
+
+    def test_072_obtain_changelog(self):
+        r = self.client.get(self.name + '/obtain_changelog')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        if data is None:
+            return
+        self.assertIsInstance(data, list)
+
+    def test_073_get_update_ops(self):
+        r = self.client.get(self.name + '/get_update_ops')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        if data is None:
+            return
+        self.assertIsInstance(data, list)
+
+    def test_074_update_info(self):
+        r = self.client.get(self.name + '/update_info')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        if data is None:
+            return
+        self.assertIsInstance(data, dict)
+
+    def test_075_trains(self):
+        r = self.client.get(self.name + '/trains')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        if data is None:
+            return
+        self.assertIsInstance(data, list)
+
+    def test_076_get_current_train(self):
+        r = self.client.get(self.name + '/get_current_train')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, str)
