8a09d4c9bf5fe897299d333b7a40dda15434dd74
William Grzybowski <wg@FreeBSD.org>
2016-08-12 14:35:44 -0300

    feat(restd): add resource and tests for backup query/sync/restore

--- src/restd/plugins/backup.py
+++ src/restd/plugins/backup.py
@@ -1,8 +1,28 @@
-from base import CRUDBase
+from base import CRUDBase, Resource
+
+
+class QueryResource(Resource):
+    name = 'query'
+    post = 'task:backup.query'
+
+
+class RestoreResource(Resource):
+    name = 'restore'
+    post = 'task:backup.restore'
+
+
+class SyncResource(Resource):
+    name = 'sync'
+    post = 'task:backup.sync'
 
 
 class BackupCRUD(CRUDBase):
     namespace = 'backup'
+    item_resources = (
+        QueryResource,
+        RestoreResource,
+        SyncResource,
+    )
 
 
 def _init(rest):
--- src/restd/tests/resources/test_backup.py
+++ src/restd/tests/resources/test_backup.py
@@ -30,6 +30,20 @@ class BackupTestCase(CRUDTestCase):
 
     def test_030_supported_providers(self):
         r = self.client.get(self.name + '/supported_providers')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, 200, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, dict)
+
+    def test_070_sync(self):
+        r = self.client.post(self.name + '/id/backuptest/sync')
+        self.assertEqual(r.status_code, 201, msg=r.text)
+        data = r.json()
+
+    def test_071_query(self):
+        r = self.client.post(self.name + '/id/backuptest/query')
+        self.assertEqual(r.status_code, 201, msg=r.text)
+
+    def test_072_restore(self):
+        r = self.client.post(self.name + '/id/backuptest/restore')
+        self.assertEqual(r.status_code, 201, msg=r.text)
+        data = r.json()
