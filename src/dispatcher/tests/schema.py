648d6b65c50616388c44a5eb3290e36932d5d14b
Wojciech Kloska <shangteus@ixsystems.com>
2016-08-15 16:16:59 +0200

    ADDED: schema validator for testing purposes

---
+++ src/dispatcher/tests/schema.py
@@ -0,0 +1,63 @@
+#+
+# Copyright 2016 iXsystems, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#####################################################################
+
+from freenas.dispatcher import validator
+from jsonschema import RefResolver
+
+SCHEMA_DEFINITIONS = None
+
+
+def __get_schema_resolver(schema):
+    return RefResolver('', schema, SCHEMA_DEFINITIONS)
+
+
+def __schema_to_list(schema):
+    return {
+        'type': 'array',
+        'items': schema,
+        'minItems': sum([1 for x in schema if 'mandatory' in x and x['mandatory']]),
+        'maxItems': len(schema)
+    }
+
+
+def verify_schema(clazz, args, strict=False):
+    if not hasattr(clazz, 'params_schema'):
+        return []
+
+    schema = __schema_to_list(clazz.params_schema)
+    val = validator.DefaultDraft4Validator(schema, resolver=__get_schema_resolver(schema))
+    if strict:
+        val.fail_read_only = True
+    else:
+        val.remove_read_only = True
+
+    return list(val.iter_errors(args))
+
+
+def load_schema_definitions(client):
+    global SCHEMA_DEFINITIONS
+    SCHEMA_DEFINITIONS = client.call_sync('discovery.get_schema')
