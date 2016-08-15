186efa92667363d00a85b18a781aaaadb4b2f0bd
William Grzybowski <wg@FreeBSD.org>
2016-08-15 10:40:42 -0300

    fix(restd): show response text for asserts in system device

--- src/restd/tests/resources/test_system_device.py
+++ src/restd/tests/resources/test_system_device.py
@@ -6,12 +6,12 @@ class SystemDeviceTestCase(RESTTestCase):
 
     def test_020_get_classes(self):
         r = self.client.get(self.name + '/get_classes')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, 200, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, list)
 
     def test_020_get_devices(self):
         r = self.client.post(self.name + '/get_devices', data=['network'])
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, 201, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, list)
