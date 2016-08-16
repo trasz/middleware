0ace552ea712111556e3c05d1d2a4b974413471e
William Grzybowski <wg@FreeBSD.org>
2016-08-16 12:18:48 -0300

    feat(restd): add cpu, disk, system and network resources to stat

--- src/restd/plugins/stat.py
+++ src/restd/plugins/stat.py
@@ -7,5 +7,33 @@ class StatResource(ProviderMixin, ResourceQueryMixin, Resource):
     get = 'rpc:stat.query'
 
 
+class StatCpuResource(ProviderMixin, ResourceQueryMixin, Resource):
+    name = 'stat/cpu'
+    provider = 'stat.cpu'
+    get = 'rpc:stat.cpu.query'
+
+
+class StatDiskResource(ProviderMixin, ResourceQueryMixin, Resource):
+    name = 'stat/disk'
+    provider = 'stat.disk'
+    get = 'rpc:stat.disk.query'
+
+
+class StatSystemResource(ProviderMixin, ResourceQueryMixin, Resource):
+    name = 'stat/system'
+    provider = 'stat.system'
+    get = 'rpc:stat.system.query'
+
+
+class StatNetworkResource(ProviderMixin, ResourceQueryMixin, Resource):
+    name = 'stat/network'
+    provider = 'stat.network'
+    get = 'rpc:stat.network.query'
+
+
 def _init(rest):
     rest.register_resource(StatResource)
+    rest.register_resource(StatCpuResource)
+    rest.register_resource(StatDiskResource)
+    rest.register_resource(StatSystemResource)
+    rest.register_resource(StatNetworkResource)
