10346e8df5e0060c4a83e9757cdf024afd577af9
William Grzybowski <wg@FreeBSD.org>
2016-08-16 12:20:14 -0300

    feat(restd): add more stat tests

--- src/restd/tests/resources/test_stat.py
+++ src/restd/tests/resources/test_stat.py
@@ -17,3 +17,51 @@ class StatTestCase(RESTTestCase):
         self.assertEqual(r.status_code, 200, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, list)
+
+
+class StatCpuTestCase(RESTTestCase):
+    name = 'stat/cpu'
+
+    def test_020_retrieve(self):
+        r = self.client.get(self.name, params={
+            'limit': 20,
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+
+
+class StatDiskTestCase(RESTTestCase):
+    name = 'stat/disk'
+
+    def test_020_retrieve(self):
+        r = self.client.get(self.name, params={
+            'limit': 20,
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+
+
+class StatSystemTestCase(RESTTestCase):
+    name = 'stat/system'
+
+    def test_020_retrieve(self):
+        r = self.client.get(self.name, params={
+            'limit': 20,
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
+
+
+class StatNetworkTestCase(RESTTestCase):
+    name = 'stat/network'
+
+    def test_020_retrieve(self):
+        r = self.client.get(self.name, params={
+            'limit': 20,
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, list)
