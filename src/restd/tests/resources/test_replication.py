d5019e1192702e1c9c98935bf67dda959c3de326
William Grzybowski <wg@FreeBSD.org>
2016-08-19 14:58:19 -0300

    fix(restd): get_replication_state is now a private method

--- src/restd/tests/resources/test_replication.py
+++ src/restd/tests/resources/test_replication.py
@@ -9,9 +9,3 @@ class ReplicationLinkTestCase(RESTTestCase):
         self.assertEqual(r.status_code, 200, msg=r.text)
         data = r.json()
         self.assertIsInstance(data, list)
-
-    def test_025_get_replication_state(self):
-        r = self.client.post(self.name + '/get_replication_state', data={
-            'partners': [],
-        })
-        self.assertEqual(r.status_code, 201, msg=r.text)
