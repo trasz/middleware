e303019e39f68a40132d64731ff26c0dd1309b50
William Grzybowski <wg@FreeBSD.org>
2016-08-22 22:03:03 +0200

    feat(restd): add a first test which will create a volume

---
+++ src/restd/tests/resources/test_001_volume.py
@@ -0,0 +1,20 @@
+from base import RESTTestCase
+
+
+class VolumeCreateTestCase(RESTTestCase):
+
+    def test_020_create(self):
+        r = self.client.get('volume/get_available_disks')
+        self.assertEqual(r.status_code, 200)
+        disks = r.json()
+        r = self.client.post('volume', data={
+            'id': 'tank',
+            'type': 'zfs',
+            'topology': {
+                'data': [{
+                    'type': 'disk',
+                    'path': disks[0],
+                }]
+            },
+        })
+        self.assertEqual(r.status_code, 201)
