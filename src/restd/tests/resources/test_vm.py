d228462b98549df3e16335847779de30b7299552
William Grzybowski <wg@FreeBSD.org>
2016-08-23 10:35:07 -0300

    feat(restd): add vm export and import tests

--- src/restd/tests/resources/test_vm.py
+++ src/restd/tests/resources/test_vm.py
@@ -23,7 +23,23 @@ class VmTestCase(CRUDTestCase):
             'config': config,
         }
 
-    def test_061_start(self):
+    def test_061_export(self):
+        r = self.client.get(self.name, params={
+            'name': 'testvm',
+        })
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        vm = data[0]
+        r = self.client.post(self.name + '/id/' + vm['id'] + '/export')
+        self.assertEqual(r.status_code, 201, msg=r.text)
+
+    def test_062_import(self):
+        r = self.client.post(self.name + '/import', data=[
+            'testvm', 'tank',
+        ])
+        self.assertEqual(r.status_code, 201, msg=r.text)
+
+    def test_064_start(self):
         r = self.client.get(self.name, params={
             'name': 'testvm',
         })
