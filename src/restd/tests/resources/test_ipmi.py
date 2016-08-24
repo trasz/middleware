1f42a424e5a1a64b7cc94e23ed99c15433a1be49
William Grzybowski <wg@FreeBSD.org>
2016-08-24 10:51:43 -0300

    feat(restd): make sure ipmi is loaded

--- src/restd/tests/resources/test_ipmi.py
+++ src/restd/tests/resources/test_ipmi.py
@@ -4,11 +4,20 @@ from base import RESTTestCase
 class IPMITestCase(RESTTestCase):
     name = 'ipmi'
 
+    def check_ipmi(self):
+        r = self.client.get(self.name + '/is_ipmi_loaded')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        if data is False:
+            self.skipTest('IPMI not loaded')
+
     def test_020_retrieve(self):
+        self.check_ipmi()
         r = self.client.get(self.name)
         self.assertEqual(r.status_code, 200, msg=r.text)
 
     def test_060_update(self):
+        self.check_ipmi()
         r = self.client.put(self.name, data=[
             0, {
                 'vlan_id': None,
